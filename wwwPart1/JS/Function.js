66d23594e1700d0ddf82b52647e39bd55211ca4a
Alisharon <ali.sharon@gmail.com>
2022-09-27 00:05:05 +1300

    new

---
+++ wwwPart1/JS/Function.js
@@ -0,0 +1,67 @@
+
+/*====================================Responsive Navigation====================================*/
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+
+
+/*====================================Submit Button for Contact/Booking-form=====================================*/
+const form = document.getElementById('containerForm', 'form');
+
+form.addEventListener('submit-btn', function handleSubmit(event) {
+  event.preventDefault();
+
+  // 👇️ Send data to server here
+
+  // 👇️ Reset form here
+  form.reset();
+});
+
+
+/*====================================BookingCalendar=====================================*/
+
+var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = (currentDateTime.getMonth() + 1);
+var date = (currentDateTime.getDate() + 1);
+
+if(date < 10) {
+  date = '0' + date;
+}
+if(month < 10) {
+  month = '0' + month;
+}
+
+var dateTomorrow = year + "-" + month + "-" + date;
+var checkinElem = document.querySelector("#checkin-date");
+/*var checkoutElem = document.querySelector("#checkout-date");*/
+
+checkinElem.setAttribute("min", dateTomorrow);
+
+checkinElem.onchange = function () {
+    checkoutElem.setAttribute("min", this.value);
+}
+
+// const form = document.getElementById('booking-form');
